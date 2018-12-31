1a7358db93923c0472252e3b41072a68840a5482
abedley <bedleyalex@gmail.com>
2018-12-31 12:31:04 -0500

    please dont judge me for this terrible but working code

--- index.js
+++ index.js
@@ -23,23 +23,49 @@ app.get('/final', function(req, res){
 app.use('/static', express.static(__dirname + '/static'));
 
 let players = {};
+let names = {};
 let next_id = 0;
 
 io.on('connection', function(socket) {
   console.log(socket.id);
   socket.on('newplayer', function(msg, fn) {
-    players[next_id] = msg.name;
-    fn(next_id);
-    next_id++;
+    if (!(msg.name in names)) {
+      players[next_id] = msg.name;
+      names[msg.name] = next_id;
+      fn(next_id);
+      next_id++;
+    } else {
+      fn(names[msg.name]);
+    }
     console.dir(players);
 
     io.emit('playerupdate', players);
   });
   socket.on('ring', function(msg) {
-    console.log("ring from "+ msg.id + "!");
+    console.log(microseconds() + " ring from "+ players[msg.id] + "!");
+  });
+  socket.on('buzzeron', function(msg) {
+    io.emit('buzzeron');
+    console.log('buzzers on');
+  });
+  socket.on('buzzeroff', function(msg) {
+    io.emit('buzzeroff');
+    console.log('buzzers off');
   });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+var start = microsecondsStart();
+
+function microsecondsStart() {
+  var hrTime = process.hrtime();
+  return ((hrTime[0] * 1000000 + hrTime[1] / 1000)/ 1000);
+}
+
+function microseconds() {
+  var hrTime = process.hrtime();
+  return ((hrTime[0] * 1000000 + hrTime[1] / 1000)/ 1000) - start;
+}
